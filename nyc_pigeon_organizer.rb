cab4ecaf40c7ebe056330683f6a66f702f31d717
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-09 21:50:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,10 @@ def nyc_pigeon_organizer(data)
   res = {}
   data.each do |info , infoVal|
     infoVal.each do |infoData , name_arr|
-      res[name_arr]
+      name_arr.each do |i|
+        res[i]
+      
     end
   end
-  
-  puts res
   return res
 end
