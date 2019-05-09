3a6ed6b18ce7b97b0c20526484f627e1683e21da
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-09 22:18:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,7 +29,7 @@ def nyc_pigeon_organizer(data)
     infoOptions.each do |option , pidgeons|
       
       pidgeons.each do |i|
-        puts"giving #{i} the var: #{infoKey} (which is #{option}"
+        puts"giving #{i} the var: #{infoKey} (which is #{option})"
         res[i] = {}
         res[i][infoKey] = option
       end
