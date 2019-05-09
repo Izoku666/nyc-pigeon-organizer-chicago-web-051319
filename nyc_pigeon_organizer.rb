768d21103a9c6d380287a9186d27f3e06dda9c80
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-09 22:04:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,6 +30,7 @@ def nyc_pigeon_organizer(data)
       pidgeons.each do |i|
         puts"listing pidgeons #{i}"
         res[i] = infoKey
+        res[i][infoKey] = option
       end
     end
   end
